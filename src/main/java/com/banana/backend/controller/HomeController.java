cdff4f2c6661a0caeb0ba12cf5a9f99407883d33
X Anshumali <kh985@ruter.no>
2018-12-18 16:52:48 +0100

    REST-3 Add FlywayDB for Database versioning.

--- src/main/java/com/banana/backend/controller/HomeController.java
+++ src/main/java/com/banana/backend/controller/HomeController.java
@@ -1,15 +1,13 @@
 package com.banana.backend.controller;
 
-import com.banana.backend.domain.Shipwreck;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
 @RequestMapping("/")
 public class HomeController {
+
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String welcome() {
         return "Welcome to HomePage";
---
+++ src/main/resources/db/migration/V2_add_Situation.sql
@@ -0,0 +1,3 @@
+INSERT INTO shipwreck(
+	id, condition, depth, description, latitude, longitude, name, year_discovered)
+	VALUES ( nextval('shipwreck_seq'),'WellPreserved', 1000, 'Well preserved wrect of TomSk ship',87.9999, 32.6666, 'TomSK Ship', 1997);
